5d224fb4804852b734c7b6984e4f46f697ff1a5a
Matias Hospital <matiashospital@gmail.com>
2018-04-05 22:57:44 -0300

    Agrega configuracion de Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyB5KU1Y95XpKfXth6YXYUT_prQXFez71bw",
+    authDomain: "loginapp-4a9c1.firebaseapp.com",
+    databaseURL: "https://loginapp-4a9c1.firebaseio.com",
+    projectId: "loginapp-4a9c1",
+    storageBucket: "",
+    messagingSenderId: "249648861424"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyB5KU1Y95XpKfXth6YXYUT_prQXFez71bw",
+    authDomain: "loginapp-4a9c1.firebaseapp.com",
+    databaseURL: "https://loginapp-4a9c1.firebaseio.com",
+    projectId: "loginapp-4a9c1",
+    storageBucket: "",
+    messagingSenderId: "249648861424"
+  }
 };
