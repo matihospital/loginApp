53967732c0429ef3ac9ff759a2db10b8b660badc
Matias Hospital <matiashospital@gmail.com>
2018-04-06 00:04:20 -0300

    Agrega funcionalidad de registro de usuarios

--- src/app/components/register-page/register-page.component.ts
+++ src/app/components/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../../services/auth.service";
 
 @Component({
   selector: 'app-register-page',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPageComponent implements OnInit {
 
-  constructor() { }
+  public email: string;
+  public pass: string;
+
+  constructor(
+    public authService: AuthService
+  ) { }
 
   ngOnInit() {
   }
 
+  onSubmitAddUser(){
+    this.authService.signIn(this.email, this.pass)
+      .then( (res)=>{
+        console.log('Usuario Registrado');
+        console.log(res);
+      }).catch( (err) => {
+        console.log(err)
+    })
+  }
+
 }
+
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth} from "angularfire2/auth";
+import * as firebase from 'firebase/app';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class AuthService {
+
+  constructor(public afAuth: AngularFireAuth) {  }
+
+  signIn(email: string, pass: string){
+    return new Promise((resolve, reject) => {
+      this.afAuth.auth.createUserWithEmailAndPassword(email,pass)
+        .then( userData => resolve(userData), err => reject(err));
+    });
+  }
+
+  logIn(email:string, pass:string){
+    return new Promise( (resolve, reject) => {
+      this.afAuth.auth.signInWithEmailAndPassword(email,pass)
+        .then( userData => resolve(userData), err => reject(err));
+    });
+  }
+
+  logOut(){
+    return this.afAuth.auth.signOut();
+  }
+
+  getAuth(){
+    return this.afAuth.authState.map (auth => auth);
+  }
+}
